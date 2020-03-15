0b4e4c71178db2301801e2dc945f9a3478555b72
Anish Mangal <anishmg@umich.edu>
2020-03-15 17:22:36 +0530

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, request
+
+from app import app
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
+def application():
+    region = 'Dharamsala'
+
+    if request.method == 'GET':
+        return render_template('need-help.html', region=region)
+    elif request.method == 'POST':
+        print(request.form)
+        return render_template('need-help.html', region=region)
+    else:
+        print('NOT A VALID SUBMISSION')
+        return render_template('need-help.html', region=region)
+
